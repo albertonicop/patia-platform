5c469746f80d4f267d1a8d8322710ab40b4e7ed0
Alberto Patjane <albertonicopat@gmail.com>
2026-06-16 16:36:27 -0600

    fix: recrear db con ticket_id

--- app/__init__.py
+++ app/__init__.py
@@ -35,6 +35,7 @@ def create_app():
     app.register_blueprint(main)
 
     with app.app_context():
+        db.drop_all()
         db.create_all()
         import sqlalchemy as sa
         with db.engine.connect() as conn:
